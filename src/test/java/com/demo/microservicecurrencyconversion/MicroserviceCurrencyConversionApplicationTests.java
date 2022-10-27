c51999a49e593ab609e9d804fa66f63b0742d7bf
Ismael Domínguez Martínez <ismaeldom11@gmail.com>
2022-10-27 17:41:27 -0500

    Commit inicial.

---
+++ src/test/java/com/demo/microservicecurrencyconversion/MicroserviceCurrencyConversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.microservicecurrencyconversion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCurrencyConversionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
