04b5d030eada77d041b57ecd0007e584fb7ad1a0
Ismael Domínguez Martínez <ismaeldom11@gmail.com>
2022-10-28 11:07:04 -0500

    Add configuracion.

--- src/main/java/com/demo/microservicecurrencyconversion/MicroserviceCurrencyConversionApplication.java
+++ src/main/java/com/demo/microservicecurrencyconversion/MicroserviceCurrencyConversionApplication.java
@@ -2,8 +2,13 @@ package com.demo.microservicecurrencyconversion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+//@EnableFeignClients("com.demo.currencyconversion")
+@EnableFeignClients
+@EnableDiscoveryClient
 public class MicroserviceCurrencyConversionApplication {
 
 	public static void main(String[] args) {
